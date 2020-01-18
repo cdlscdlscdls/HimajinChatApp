34c875e56deae138817651dc6c949b260b52d7f4
jotaro.nishime <jnishime@jotaronishimenoMacBook-Pro.local>
2020-01-18 16:41:00 +0900

    アクティビティからボタンを作成する処理追加

--- app/src/main/java/com/example/himajinchat/MainActivity.kt
+++ app/src/main/java/com/example/himajinchat/MainActivity.kt
@@ -2,11 +2,27 @@ package com.example.himajinchat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.constraintlayout.widget.ConstraintLayout
+import android.widget.Button
+import android.graphics.Color
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Create Button 
+        val constraintLayout = findViewById(R.id.constraintLayout) as ConstraintLayout
+        val button = Button(this)
+        button.layoutParams = ConstraintLayout.LayoutParams(ConstraintLayout.LayoutParams.WRAP_CONTENT, ConstraintLayout.LayoutParams.WRAP_CONTENT)
+        button.text = "Click me"
+        button.setOnClickListener(View.OnClickListener {
+            button.text = "You just clicked me"
+        })
+        button.setBackgroundColor(Color.GREEN)
+        button.setTextColor(Color.RED)
+        constraintLayout.addView(button);
     }
 }
