126e751713b788a5c068178836838682c899819f
jotaro.nishime <jnishime@jotaronishimenoMacBook-Pro.local>
2020-01-19 17:32:59 +0900

    Firestoreのadd,get処理 動作確認

--- app/src/main/java/com/example/himajinchat/MainActivity.kt
+++ app/src/main/java/com/example/himajinchat/MainActivity.kt
@@ -6,13 +6,35 @@ import androidx.constraintlayout.widget.ConstraintLayout
 import android.widget.Button
 import android.graphics.Color
 import android.view.View
-import android.content.Intent
+import com.google.firebase.firestore.FirebaseFirestore
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        val TAG = "Firesotre"
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Access a Cloud Firestore instance from your Activity
+        val db = FirebaseFirestore.getInstance()
+        // Create a new user with a first and last name
+        val user = hashMapOf(
+            "first" to "Ada",
+            "last" to "Lovelace",
+            "born" to 1815
+        )
+
+        // Add a new document with a generated ID
+        db.collection("users")
+            .add(user)
+            .addOnSuccessListener { documentReference ->
+                Log.d(TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
+            }
+            .addOnFailureListener { e ->
+                Log.w(TAG, "Error adding document", e)
+            }
 
         // Create Button 
         val constraintLayout = findViewById(R.id.mainLayout) as ConstraintLayout
@@ -20,6 +42,16 @@ class MainActivity : AppCompatActivity() {
         button.layoutParams = ConstraintLayout.LayoutParams(ConstraintLayout.LayoutParams.WRAP_CONTENT, ConstraintLayout.LayoutParams.WRAP_CONTENT)
         button.text = "Click me"
         button.setOnClickListener(View.OnClickListener {
+            db.collection("users")
+                .get()
+                .addOnSuccessListener { result ->
+                    for (document in result) {
+                        Log.d(TAG, "${document.id} => ${document.data}")
+                    }
+                }
+                .addOnFailureListener { exception ->
+                    Log.w(TAG, "Error getting documents.", exception)
+                }
             setContentView(R.layout.activity_sub)
         })
         button.setBackgroundColor(Color.GREEN)
